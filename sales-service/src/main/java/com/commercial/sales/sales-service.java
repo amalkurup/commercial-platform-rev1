3e9feb8054b3ac4bad2e47865bbdafad2c6fd5b4
default <default>
2019-02-12 16:45:06 +0530

    Adding initial code structure

---
+++ sales-service/src/main/java/com/commercial/sales/sales-service.java
@@ -0,0 +1,12 @@
+package com.commercial.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sales-service {
+
+	public static void main(String[] args) {
+		SpringApplication.run(sales-service.class, args);
+	}
+}
---
+++ sales-service/src/test/java/com/commercial/sales/sales-serviceTests.java
@@ -0,0 +1,16 @@
+package com.commercial.sales;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class sales-serviceTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
